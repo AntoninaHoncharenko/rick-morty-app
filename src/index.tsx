69bc1b7915fb38e038a3da68a4872af63a28f7f9
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2023-03-18 12:53:51 +0200

    bugfix

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/rick-morty-app">
+    <BrowserRouter>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
